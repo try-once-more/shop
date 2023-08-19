831585700e033e05928023030dcbb86b31b55ee3
Vadzim Zianiuk <Vadzim.Zianiuk@gmail.com>
2023-08-19 21:46:51 +0200

    Changed moved appTitle to App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
 import { CartListComponent } from "./cart/components/cart-list/cart-list.component";
 import { ProductListComponent } from "./products/components/product-list/product-list.component";
 
@@ -9,6 +9,11 @@ import { ProductListComponent } from "./products/components/product-list/product
     styleUrls: ["./app.component.css"],
     imports: [CartListComponent, ProductListComponent]
 })
-export class AppComponent {
-    title = "shop";
+export class AppComponent implements AfterViewInit {
+    private title = "shop";
+    @ViewChild("appTitle", { static: true }) appTitle!: ElementRef<HTMLHeadingElement>;
+
+    ngAfterViewInit() {
+        this.appTitle.nativeElement.textContent = this.title;
+    }
 }
