5daa00bf6299e05ee4cf27e163b4b2a9cc3c911c
Vadzim Zianiuk <Vadzim.Zianiuk@gmail.com>
2023-08-19 10:15:31 +0200

    Added total cost of products added to the cart

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -7,11 +7,14 @@ import { ProductModel } from "src/app/products/models/product.model";
 export class CartService {
     private cart: Map<ProductModel, number> = new Map();
     private _totalQuantity: WritableSignal<number> = signal(0);
+    private _totalCost: WritableSignal<number> = signal(0);
     totalQuantity: Signal<number> = this._totalQuantity.asReadonly();
+    totalCost: Signal<number> = this._totalCost.asReadonly();
 
     addToCart(product: ProductModel) {
         this.cart.set(product, (this.cart.get(product) || 0) + 1);
         this._totalQuantity.update(value => ++value);
+        this._totalCost.update(value => value + product.price);
     }
 
     getCartItems(): [ProductModel, number][] {
