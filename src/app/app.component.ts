411cff366b674548e0ce4c6c3e31db81780e16ae
Vadzim Zianiuk <Vadzim.Zianiuk@gmail.com>
2023-08-19 22:24:50 +0200

    Added current version to app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
 import { CartListComponent } from "./cart/components/cart-list/cart-list.component";
 import { ProductListComponent } from "./products/components/product-list/product-list.component";
-
+import { environment } from "../environment";
 @Component({
     standalone: true,
     selector: "app-root",
@@ -10,7 +10,8 @@ import { ProductListComponent } from "./products/components/product-list/product
     imports: [CartListComponent, ProductListComponent]
 })
 export class AppComponent implements AfterViewInit {
-    private title = "shop";
+
+    private title = `Shop v${environment.version}`;
     @ViewChild("appTitle", { static: true }) appTitle!: ElementRef<HTMLHeadingElement>;
 
     ngAfterViewInit() {
