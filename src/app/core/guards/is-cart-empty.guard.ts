5f1f245b2a24a43f588e0709f64e14939d41f9c9
Vadzim Zianiuk <vadzim.zianiuk@gmail.com>
2023-09-12 08:07:44 +0200

    fixed navigation to the previous page if isCartEmptyGuard denied access

--- src/app/core/guards/is-cart-empty.guard.ts
+++ src/app/core/guards/is-cart-empty.guard.ts
@@ -1,16 +1,16 @@
 import { inject } from '@angular/core';
+import { Location } from '@angular/common';
 import { CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { CartService } from "src/app/cart/services/cart.service";
 import { firstValueFrom } from 'rxjs';
 
 export const isCartEmptyGuard: CanActivateFn = 
     async (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-        const router = inject(Router);
         const products = await firstValueFrom(inject(CartService).getProducts());
         if (products.length > 0) {
             return true;
         }
 
-        router.navigateByUrl("");
+        inject(Location).back();
         return false;
 };
