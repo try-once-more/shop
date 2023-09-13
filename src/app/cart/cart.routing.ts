20ef92905ce53bc378e3669a2bd47342921c75e3
Vadzim Zianiuk <vadzim.zianiuk@gmail.com>
2023-09-13 15:44:29 +0200

    moved ProcessOrderComponent

--- src/app/cart/cart.routing.ts
+++ src/app/cart/cart.routing.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { CartListComponent } from "./components/cart-list/cart-list.component";
-import { ProcessOrderComponent } from "./components/process-order/process-order.component";
+import { ProcessOrderComponent } from "../order/components/process-order/process-order.component";
 import { isCartEmptyGuard } from "../core/guards/is-cart-empty.guard";
 
 
