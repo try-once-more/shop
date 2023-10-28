b3f687658b28a0597e9288b6d25e3cf53b652426
Vadzim Zianiuk <vadzim.zianiuk@gmail.com>
2023-10-28 11:42:02 +0200

    Added ProcessOrderComponent form

--- src/app/order/components/process-order/process-order.component.ts
+++ src/app/order/components/process-order/process-order.component.ts
@@ -1,9 +1,46 @@
-import { Component } from "@angular/core";
+import { NgClass, NgIf } from "@angular/common";
+import { Component, OnInit, inject } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
     selector: "app-process-order",
     standalone: true,
-    templateUrl: "./process-order.component.html"
+    templateUrl: "./process-order.component.html",
+    imports: [ReactiveFormsModule, NgIf, NgClass]
 })
-export class ProcessOrderComponent {
+export class ProcessOrderComponent implements OnInit {
+    orderForm!: FormGroup;
+    showDeliveryAddress = false;
+
+    private readonly formBuilder = inject(FormBuilder);
+
+    ngOnInit() {
+        const upperCasePattern = /^\p{Lu}/u; //Any uppercase Unicode letter as the first character.
+        this.orderForm = this.formBuilder.group({
+            firstName: ["", [Validators.required, Validators.pattern(upperCasePattern)]],
+            lastName: [""],
+            email: ["", [Validators.required, Validators.email]],
+            phone: [""],
+            delivery: [this.showDeliveryAddress],
+            address: [""]
+        });
+    }
+
+    toggleDelivery() {
+        this.showDeliveryAddress = !this.showDeliveryAddress;
+
+        if (!this.showDeliveryAddress) {
+            this.orderForm.value.address.clearValidators();
+            this.orderForm.value.address.setValue("");
+        } else {
+            this.orderForm.value.address.setValidators([Validators.required]);
+        }
+    }
+
+    submitOrder() {
+        if (this.orderForm.valid) {
+            //TODO: Add submit order
+            console.log("Submitted!", this.orderForm.value);
+        }
+    }
 }
