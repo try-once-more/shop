b64fde1daa6f6ee4e0354f69c391b1f8f7a36064
Vadzim Zianiuk <vadzim.zianiuk@gmail.com>
2023-10-31 09:02:51 +0100

    fixed ProductComponent tests

--- src/app/products/components/product/product.component.spec.ts
+++ src/app/products/components/product/product.component.spec.ts
@@ -40,35 +40,42 @@ describe("ProductComponent", () => {
         expect(compiled.querySelector(".card-subtitle").textContent).toContain("Test Description");
     });
 
+    it("Buy button should be displayed", () => {
+        const buyButton = fixture.debugElement.query(By.css("button.btn-outline-primary"));
+        expect(buyButton).toBeDefined();
+    });
+
     it("should emit addToCart event on Buy button click", () => {
         const addToCartSpy = spyOn(component.addToCart, "emit");
         const buyButton = fixture.debugElement.query(By.css("button.btn-outline-primary"));
 
-        expect(buyButton).toBeDefined();
         buyButton.triggerEventHandler("click");
 
         expect(addToCartSpy).toHaveBeenCalledOnceWith(product);
     });
 
+    it("View button should be displayed", () => {
+        const viewButton = fixture.debugElement.query(By.css("button i.bi.bi-eye")).parent;
+        expect(viewButton).toBeDefined();
+    });
+
     it("should navigate to product page when View button is clicked", () => {
         const viewButton = fixture.debugElement.query(By.css("button i.bi.bi-eye")).parent;
-
-        expect(viewButton).toBeDefined();
-        viewButton!.triggerEventHandler("click");
-        fixture.detectChanges();
-
         const routerLinkInstance = viewButton!.injector.get(RouterLink);
+        
         expect(routerLinkInstance["commands"]).toEqual(["/product", product.id]);
     });
 
-    it("if the user is not an admin, Edit button shouldn't be displayed", () => {
-        const editButton = fixture.debugElement.query(By.css("button i.bi.bi-pencil"));
-        expect(editButton).toBeNull();
-    });
+    describe("if the user is not an admin", () => {
+        it("Edit button shouldn't be displayed", () => {
+            const editButton = fixture.debugElement.query(By.css("button i.bi.bi-pencil"));
+            expect(editButton).toBeNull();
+        });
 
-    it("if the user is not an admin, Delete button shouldn't be displayed", () => {
-        const deleteButton = fixture.debugElement.query(By.css("button i.bi.bi-trash"));
-        expect(deleteButton).toBeNull();
+        it("Delete button shouldn't be displayed", () => {
+            const deleteButton = fixture.debugElement.query(By.css("button i.bi.bi-trash"));
+            expect(deleteButton).toBeNull();
+        });
     });
 
     describe("if the user is an admin", () => {
@@ -103,11 +110,8 @@ describe("ProductComponent", () => {
 
         it("should navigate to edit page when Edit button is clicked", () => {
             const editButton = fixture.debugElement.query(By.css("button i.bi.bi-pencil")).parent;
-
-            editButton!.triggerEventHandler("click");
-            fixture.detectChanges();
-
             const routerLinkInstance = editButton!.injector.get(RouterLink);
+            
             expect(routerLinkInstance["commands"]).toEqual(["/admin/product/edit", product.id]);
         });
     });
